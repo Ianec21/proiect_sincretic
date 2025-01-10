2a9b18b16657d0f4d5078cd1cd86e528eb384cb2
Ianec21 <jizdan.ianec17@gmail.com>
2025-01-10 16:28:01 +0200

    changed footer description

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,14 +45,14 @@ export default function RootLayout({
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
-                  Powered by{" "}
+                  Creat de{" "}
                   <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                    href="https://github.com/Ianec21"
                     target="_blank"
                     className="font-bold hover:underline"
                     rel="noreferrer"
                   >
-                    Supabase
+                    Jizdan Ianec
                   </a>
                 </p>
                 <ThemeSwitcher />
